08fcc62b65d86a788c6ec4ab41c697e39c0cf722
George Gates <ggates@navisite.com>
2020-11-08 14:58:50 -0500

    Update event_calendar.rb

--- lib/event_calendar.rb
+++ lib/event_calendar.rb
@@ -49,11 +49,11 @@ module EventCalendar
     
     # Get the events overlapping the given start and end dates
     def events_for_date_range(start_d, end_d, find_options = {})
-      self.where(find_options).find(
-        :all,
+      
+      self.where(find_options,
         :conditions => [ "(? <= #{self.quoted_table_name}.#{self.end_at_field}) AND (#{self.quoted_table_name}.#{self.start_at_field}< ?)", start_d.to_time.utc, end_d.to_time.utc ],
         :order => "#{self.quoted_table_name}.#{self.start_at_field} ASC"
-      )
+      ).load
     end
     
     # Create the various strips that show events.
